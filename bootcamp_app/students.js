47a71e82712232944bf5d82c58f11120940b744c
Ray Yiu <rayyiu66@gmail.com>
2021-03-03 22:02:07 -0800

    used parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -8,8 +8,8 @@ const pool = new Pool({
 });
 
 const cohortName = process.argv[2];
-const max = process.argv[3] || 5;
-const values = [`%${cohortName}%`, max];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
 
 
 pool.query(`SELECT students.id as student_id, students.name as name, cohorts.name as cohort
